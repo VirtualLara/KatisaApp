b1f32976c4c68934f24e29096b4ac42f0c842268
unknown <virtual_lara@hotmail.com>
2021-04-14 19:56:50 -0500

    cambios-14-04-21

---
+++ Componentes/search/Search.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { Searchbar } from "react-native-paper";
+
+export default function Search() {
+  return (
+    <View>
+      <Searchbar />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
